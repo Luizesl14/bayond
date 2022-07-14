ced10a8179f8c9619583e83f951acda527f2fc9e
Luiz Augusto <luiz.develop@gmail.com>
2022-07-13 21:44:32 -0300

    Created project

---
+++ src/main/java/com/dvp/bayond/BayondApplication.java
@@ -0,0 +1,13 @@
+package com.dvp.bayond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayondApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dvp/bayond/BayondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvp.bayond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayondApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
